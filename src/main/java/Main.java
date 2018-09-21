6519c026745c5f8d21853cc13fd77a7c2a7cef72
anton <tinagreen@mail.ru>
2018-09-21 14:09:39 +0300

    write a program which return "fizz" if the number is a multiplier of 3, return "buzz" if its multiplier of 5 and return "fizzbuzz" if the number is divisible by both 3 and 5

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int nums[] = new int[100];
+        for (int i = 1; i <= nums.length; i++) {
+            if (i % 15 == 0) {
+                System.out.println("FIZZBUZZ");
+            } else if (i % 3 == 0) {
+                System.out.println("FIZZ");
+            } else if (i % 5 == 0) {
+                System.out.println("BUZZ");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
