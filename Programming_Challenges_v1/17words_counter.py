4f303c675f5e60ff29de7db89086ca1fa3da9def
stank <tinagreen@mail.ru>
2020-02-04 13:44:55 +0300

    method uses function syntax and regex functionality

--- Programming_Challenges_v1/17words_counter.py
+++ Programming_Challenges_v1/17words_counter.py
@@ -1,10 +1,28 @@
-# lvl 1: using function
+# # lvl 1: using function
+#
+#
+# def word_counter(amount=0):
+#     for i in range(len(input().split())):
+#         amount += 1
+#     return amount
+#
+#
+# print(word_counter())
+
+
+# lvl 2: using function + regex
+
+import re
+
+wordslist = input()
 
 
 def word_counter(amount=0):
-    for i in range(len(input().split())):
+    res = len(re.findall(r'\w+', wordslist))
+    for i in range(res):
         amount += 1
     return amount
 
 
 print(word_counter())
+
