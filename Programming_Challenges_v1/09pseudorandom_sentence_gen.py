7b393189713a5fa4cccbbcef975f2d3ef6263111
stank <tinagreen@mail.ru>
2020-02-06 15:35:07 +0300

    Path problem is resolved. No dir for files with different contents and extensions.

--- Programming_Challenges_v1/09pseudorandom_sentence_gen.py
+++ Programming_Challenges_v1/09pseudorandom_sentence_gen.py
@@ -1,11 +1,17 @@
 import re
-# import os
+import os
 import random
 
-# how to resolve path-problem?
-file = open("C:\\Users\\stank\\PycharmProjects\\CodePuzzles\\Programming_Challenges_v1\\sources\\sentences.txt")
-text = ''.join(file.readlines())
-sentences = re.split(r' *[\.\?!][\'"\)\]]* *', text)
 
-# print(os.getcwd())
-print(random.choice(sentences) + ".")
+for cur_dir, subdirs, files in os.walk("C:"):
+    for file in files:
+        if file.endswith(".txt"):
+            # print("You`ve found the text file.")
+            file = open(os.path.abspath("sentences.txt"))
+            # print("You`ve opened the text file.")
+            text = ''.join(file.readlines())
+            sentences = re.split(r' *[\.\?!][\'"\)\]]* *', text)
+            print(random.choice(sentences) + ".")
+            break
+
+
