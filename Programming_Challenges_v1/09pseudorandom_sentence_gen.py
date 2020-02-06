f5593f7701dc1f307394525cddd834a0a45fece9
stank <tinagreen@mail.ru>
2020-02-06 13:26:59 +0300

    Path problem is unresolved. Added dir for files with different contents and extensions.

---
+++ Programming_Challenges_v1/09pseudorandom_sentence_gen.py
@@ -0,0 +1,11 @@
+import re
+import os
+import random
+
+# how to resolve path-problem?
+file = open("C:\\Users\\stank\\PycharmProjects\\CodePuzzles\\Programming_Challenges_v1\\sources\\sentences.txt")
+text = ''.join(file.readlines())
+sentences = re.split(r' *[\.\?!][\'"\)\]]* *', text)
+
+print(os.getcwd())
+print(random.choice(sentences) + ".")
