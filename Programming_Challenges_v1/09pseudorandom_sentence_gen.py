814be73096026058dfdaee8427fe0acafe7953da
stank <tinagreen@mail.ru>
2020-02-06 16:02:44 +0300

    Path problem is resolved. Created dir for files with different contents and extensions.

--- Programming_Challenges_v1/09pseudorandom_sentence_gen.py
+++ Programming_Challenges_v1/09pseudorandom_sentence_gen.py
@@ -2,12 +2,12 @@ import re
 import os
 import random
 
-
+# solution works for win
 for cur_dir, subdirs, files in os.walk("C:"):
     for file in files:
         if file.endswith(".txt"):
             # print("You`ve found the text file.")
-            file = open(os.path.abspath("sentences.txt"))
+            file = open(os.path.abspath("../Programming_Challenges_v1/sources/sentences.txt"))
             # print("You`ve opened the text file.")
             text = ''.join(file.readlines())
             sentences = re.split(r' *[\.\?!][\'"\)\]]* *', text)
